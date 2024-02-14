158e2820f7d9753d75471b78c963c1ac7ff3080d
gilberto barbato <gibarbato@gmail.com>
2024-02-14 09:38:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function somar() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 + num2;
+    document.getElementById("result").value = result;
+}
+
+function subtrair() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 - num2;
+    document.getElementById("result").value = result;
+}
+
+function multiplicar() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 * num2;
+    document.getElementById("result").value = result;
+}
+
+function dividir() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    if (num2 === 0) {
+        alert("Não é possível dividir por zero!");
+        return;
+    }
+    var result = num1 / num2;
+    document.getElementById("result").value = result;
+}
